f7d067e41002124f1ffbaa7dbc4e1972534fdd18
NandhiniJaidev <nandhinijaidev2019@gmail.com>
2022-08-25 20:55:11 +0530

    Done

---
+++ src/test/java/org/samgit/SampleGit.java
@@ -0,0 +1,12 @@
+package org.samgit;
+
+public class SampleGit {
+	public static void main(String[] args) 
+	{
+		double a=2.54, b=3.643,sum=a+b, avg=sum/2;
+		System.out.println("Sum = : "+sum);
+		System.out.println("Average = : "+avg);
+						
+	}
+
+}
